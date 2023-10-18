cc8e736c63ed5c661136e6773c025f5bdb27e696
joaquinvallespir <joaquinvallespir@gmail.com>
2023-10-18 14:52:11 -0300

    demo

--- demo.js
+++ demo.js
@@ -1,17 +1,19 @@
-function esVocal(letra) {
-    // Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”.
-    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
-    // Si no es vocal, tambien debe retornar "Dato incorrecto".
-    // Tu código:
-    if(letra.length===2)
-    console.log("Dato incorrecto");
-    else if (letra == 'a' || letra == 'A' || letra == 'e' || letra == 'E' || letra == 'i' || letra == 'I' || letra == 'o' || letra == 'O' || letra == 'u' || letra == 'U')
-       console.log("Es vocal");
-    else
-    {
-       console.log("Dato incorrecto");
-        return "Dato incorrecto"
-    }
- }
-
- console.log(esVocal('l'));
+function continueStatement(num) 
+{
+   let contador = 0 ;
+   let nuevoNum = num;
+   let array = [];
+   while(contador<10)
+      {
+         if(contador===5)
+         {
+            contador++;
+            continue;
+         }
+         nuevoNum += 2;
+         array.push(nuevoNum);
+         contador++;
+      }
+   return array;
+}
+ console.log(continueStatement(50));
